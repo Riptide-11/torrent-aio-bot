afa1d63ed76ec7c22836e064753f14652628cbce
Riptide-11 <69417017+Riptide-11@users.noreply.github.com>
2020-08-27 22:03:35 +0530

    Update Drive.js

--- web/src/screens/Drive.js
+++ web/src/screens/Drive.js
@@ -21,7 +21,7 @@ export default function Drive() {
       </h1>
 <b>
           For streaming files register on our <a href="https://www.bingersbrowse.ml">Site</a>
-</b>
+</b>-------------
 <i>
             And ping any admin to get access or join <a href="https://tx.me/bingersbrowse">telegram group</a>
 </i>
